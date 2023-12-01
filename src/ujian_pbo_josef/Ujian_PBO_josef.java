1577d4ce360dbafed301d8a58e7fd71adbab0d55
YPAB-LAB-RPL <YPAB-LAB-RPL@192.168.56.1>
2023-11-30 22:58:01 -0800

    design1

---
+++ src/ujian_pbo_josef/Ujian_PBO_josef.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ujian_pbo_josef;
+
+/**
+ *
+ * @author YPAB-LAB-RPL
+ */
+public class Ujian_PBO_josef {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
